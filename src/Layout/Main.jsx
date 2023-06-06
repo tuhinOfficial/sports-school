19bfd6beaed4f84e8be6b2223b2acffa27b7bec1
tuhinOfficial <tuhin.official100@gmail.com>
2023-06-06 22:41:43 +0600

    design Header

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navigation from '../SharedPages/Navigation/Navigation';
 
 const Main = () => {
     return (
         <>
+            <Navigation></Navigation>
             <Outlet></Outlet>
         </>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './Routes/Router.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Router.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-  </React.StrictMode>,
-)
+    <div className="max-w-screen-xl mx-auto">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  </React.StrictMode>
+);
