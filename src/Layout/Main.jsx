8b7ec3b58703c781d1607394a9da70ab705ff7c1
tuhinOfficial <tuhin.official100@gmail.com>
2023-06-06 21:58:01 +0600

    Setup Router

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const Main = () => {
+    return (
+        <>
+            <Outlet></Outlet>
+        </>
+    );
+};
+
+export default Main;
