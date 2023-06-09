1a6df331531d005e7d46f43a0045fc587037e306
tuhinOfficial <tuhin.official100@gmail.com>
2023-06-09 22:14:16 +0600

    Set Loading At Private Route

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,23 +1,26 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../Prividers/AuthProvider';
-import { Navigate, useLocation, useNavigate } from 'react-router-dom';
-import { Spinner } from '@material-tailwind/react';
+import React, { useContext } from "react";
+import { AuthContext } from "../Prividers/AuthProvider";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import { Spinner } from "@material-tailwind/react";
 
-const PrivateRoute = ({children}) => {
-    const {user ,loading} = useContext(AuthContext);
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
 
-    const navigate = useNavigate();
-    const location = useLocation();
+  const location = useLocation();
 
-    if (loading) {
-        <Spinner color="blue" />
-    }
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Spinner className="h-8 w-8" color="blue" />
+      </div>
+    );
+  }
 
-    if (user?.email) {
-        return children;
-    }
+  if (user?.email) {
+    return children;
+  }
 
-    return <Navigate to="/login" state={{ from: location }} replace />;
+  return <Navigate to="/login" state={{ from: location }} replace />;
 };
 
 export default PrivateRoute;
